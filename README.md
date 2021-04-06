00e9b3cba3a1076373840d8e421809de0647507f
Connor Peet <connor@peet.io>
2021-04-06 15:04:04 -0700

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# js-debug-proxy-requestor
+
+Creates a command that requests the CDP proxy for the active debug session. See:
+
+- https://github.com/microsoft/vscode-js-debug/pull/964
+- https://github.com/microsoft/vscode-js-debug/issues/893
---
+++ extension.js
@@ -0,0 +1,35 @@
+const vscode = require('vscode');
+
+function activate(context) {
+  let disposable = vscode.commands.registerCommand(
+    'js-debug-proxy-requestor.getProxy',
+    async () => {
+      const session = vscode.debug.activeDebugSession;
+      if (!session) {
+        vscode.window.showErrorMessage('No active debug session');
+        return;
+      }
+
+      try {
+        const addr = await vscode.commands.executeCommand(
+          'extension.js-debug.requestCDPProxy',
+          session.id,
+        );
+        const formed = `ws://${addr.host}:${addr.port}`;
+        vscode.env.clipboard.writeText(formed);
+        vscode.window.showInformationMessage(`Address copied to your clipboard (${formed})`);
+      } catch (e) {
+        vscode.window.showErrorMessage(e.message);
+      }
+    },
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate,
+};
